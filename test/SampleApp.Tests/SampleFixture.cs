12bc431acb472ce6a2631bf32474ee5cbcb8c218
MiguelAlho <alho@miguelaho.com>
2016-06-17 18:02:07 +0100

    played a little with the test projects (specflow setup, Autofixture + fluntassertions + nunit 3)

---
+++ test/SampleApp.Tests/SampleFixture.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace SampleApp.Tests
+{
+
+    [TestFixture]
+    public class SampleFixture
+    {
+        Fixture fixture;
+        int num1;
+        int num2;
+
+        [SetUp]
+        public void ShouldDoSomethingCoolAndWhatNot()
+        {
+            fixture = new Fixture();
+            num1 = fixture.Create<int>();
+            num2 = fixture.Create<int>();
+        }
+
+        [Test]
+        public void DoSomethingActuallySumsStuffUp()
+        {
+            var result = num1 + num2;
+            result.Should().Be(num1 + num2);
+        }
+    }
+}
